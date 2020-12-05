1b8bf1afe7c28c98b01a32f95fd815055841a0f2
Ivan K <vivenchik@gmail.com>
2020-12-05 10:41:10 +0300

    initall commit

---
+++ app/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Waiting for postgres..."
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  sleep 0.1
+done
+echo "PostgreSQL started"
+
+python manage.py makemigrations main
+python manage.py migrate
+
+exec "$@"
